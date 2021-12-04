663e719ee4f36898b8440d0f587089889a7241cc
avc0021 <avc0021@gmail.com>
2021-12-04 15:10:34 -0600

    save function add and alignment issues fixed

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 // Current date moment object
 var current = moment().format('MMMM Do YYYY');
+var saveButtonEl = document.querySelector ("#save")
 
 // Displays date on page
 var displayCurrent = document.getElementById('currentDay');
 currentDay.innerHTML = current;
 
-function savebtn () {
-    alert('saved!');
-    
-}
-console.log(savebtn);
+//function savebtn () {
+    //alert('saved!');
+    //localStorage.setItem
+//}
 
+
+
+$(".saveBtn").click(function(event) {
+    event.preventDefault();
+    var textInput = $(this).siblings(".description").val();
+    console.log(textInput);
+    localStorage.setItem("1", textInput)
+}
+
+)
 
